0efc140576c8e678ffe6fbf957aba9469785629f
Cal-Rex <andy.gibb@healthcareskills.com>
2022-11-19 12:51:35 +0000

    add validator that checks the ength of sales data input

--- run.py
+++ run.py
@@ -11,10 +11,41 @@ CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('love_sandwiches')
+# checks API Works:
+# sales = SHEET.worksheet('sales')
+#
+# data = sales.get_all_values()
+#
+# print(data)
 
-sales = SHEET.worksheet('sales')
 
-data = sales.get_all_values()
+def get_sales_data():
+    """
+    Get sales figures input from user
+    """
+    print("please enter sales data from market")
+    print("data should be entered as 6 numerical values")
+    print("with no spaces by commas.")
+    print("Example: 1,2,3,4,5,6\n")
 
-print(data)
+    data_str = input("Enter your data here: ")
+    # print(f"\nThe data provided is {data_str}")
+    sales_data = data_str.split(",")
+    # print(f"supplied data has now been converted to: {sales_data}")
+    validate_data(sales_data)
 
+
+def validate_data(values):
+    """
+    docstring 
+    """
+    try:
+        if len(values) != 6:
+            raise ValueError(
+                f"6 values are required, you provided {len(values)}"
+            )
+    except ValueError as e:
+        print(f"invalid data: {e}, please try again.\n")
+
+
+get_sales_data()
